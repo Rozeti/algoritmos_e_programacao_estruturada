c426c9f20cd4923f47a41413039d7607efc19931
Mateus Tirulli Rozeti <129351985+Rozeti@users.noreply.github.com>
2023-08-30 15:11:58 -0300

    Update ex3.c

--- Operadores logicos/ex3.c
+++ Operadores logicos/ex3.c
@@ -2,7 +2,7 @@
 
 main(){
 
-    int num1, num2, num3, maior, menor, medio;
+    int num1, num2, num3;
 
     printf("Digite o primeiro numero: ");
     scanf("%d", &num1);
@@ -10,56 +10,25 @@ main(){
     scanf("%d", &num2);
     printf("Digite o terceiro numero: ");
     scanf("%d", &num3);
+    fflush(stdin);
 
-    if (num1 > (num2, num3) && num2 < (num1, num3))
-    {
-        maior = num1;
-        medio = num3;
-        menor = num2;
-
-        printf("Digite o primeiro numero: %d, %d, %d", menor, medio, maior);
+    if ((num1 > num2 && num1 > num3 && num2 < num3)){
+        printf("A ordem ascendente dos numeros que voce digitou foi: %d, %d, %d", num2, num3, num1);
     }
-    else if (num1 > (num2, num3) && num3 < (num2, num1))
-    {
-        maior = num1;
-        medio = num2;
-        menor = num3;
-
-        printf("Digite o primeiro numero: %d, %d, %d", menor, medio, maior);
+    else if (num1 > num2 && num1 > num3 && num2 > num3){
+        printf("A ordem ascendente dos numeros que voce digitou foi: %d, %d, %d", num3, num2, num1);
     }
-    else if (num2 > (num1, num3) && num1 < (num2, num3))
-    {
-        maior = num2;
-        medio = num3;
-        menor = num1;
-
-        printf("Digite o primeiro numero: %d, %d, %d", menor, medio, maior);
+    else if ((num2 > num1 && num2 > num3 && num1 < num3)){
+        printf("A ordem ascendente dos numeros que voce digitou foi: %d, %d, %d", num1, num3, num2);
     }
-    else if (num2 > (num1, num3) && num3 < (num1, num2))
-    {
-        maior = num2;
-        medio = num1;
-        menor = num3;
-
-        printf("Digite o primeiro numero: %d, %d, %d", menor, medio, maior);
+    else if (num2 > num1 && num2 > num3 && num1 > num3){
+        printf("A ordem ascendente dos numeros que voce digitou foi: %d, %d, %d", num3, num1, num2);
     }
-    else if (num3 > (num1, num2) && num1 < (num2, num3))
+    else if ((num3 > num1 && num3 > num2 && num1 < num2))
     {
-        maior = num3;
-        medio = num2;
-        menor = num1;
-
-        printf("Digite o primeiro numero: %d, %d, %d", menor, medio, maior);
+        printf("A ordem ascendente dos numeros que voce digitou foi: %d, %d, %d", num1, num2, num3);
     }
-    else if (num3 > (num1, num2) && num2 < (num1, num3))
-    {
-        maior = num3;
-        medio = num1;
-        menor = num2;
-
-        printf("Digite o primeiro numero: %d, %d, %d", menor, medio, maior);
+    else if (num3 > num1 && num3 > num2 && num1 > num2){
+        printf("A ordem ascendente dos numeros que voce digitou foi: %d, %d, %d", num2, num1, num3);
     }
-    
-    
-    
 }

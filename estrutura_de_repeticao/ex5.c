0888fd7c602710af5777d1fb49adcfa233694729
Mateus Tirulli Rozeti <129351985+Rozeti@users.noreply.github.com>
2023-09-18 13:55:03 -0300

    Update ex5.c

--- estrutura_de_repeticao/ex5.c
+++ estrutura_de_repeticao/ex5.c
@@ -6,5 +6,5 @@ main(){
     for (num = 2; num <= 100; num += 2){
         soma += num;
     }
-    printf("\n-> A soma eh: %d", soma);   
+    printf("\n-> A soma dos primerios numeros pares eh: %d", soma);   
 }

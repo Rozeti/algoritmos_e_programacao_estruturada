813a97e3ee73b2232c310f42e016afa1e657d705
Mateus Tirulli Rozeti <129351985+Rozeti@users.noreply.github.com>
2023-09-19 21:34:16 -0300

    Update ex1.c

--- lista_avaliativa_1/ex1.c
+++ lista_avaliativa_1/ex1.c
@@ -1,10 +1,22 @@
 #include<stdio.h>
+#include<math.h>
 
 main(){
     float num, funcao;
+    int raiz;
 
     printf("-> Digite o um valor para 'X', para encontrarmos o valor da funcao f(x): ");
     scanf("%f", &num);
-    funcao = (5 * num + 3) / (num - 4);
-    printf("-> O resultado para a f(x) eh igual a: %.2f", funcao);
+
+    funcao = (5 * num + 3);
+    raiz = sqrt(pow(num, 2) - 16);
+
+    while (raiz < 0){
+        printf("\n-> Com o valor informado para X nao eh possivel encontrar o valor da funcao. Digite outro valor. Caso queira digite outro valor: ");
+        scanf("%f", &num);
+        raiz = sqrt(pow(num, 2) - 16);
+    }if (raiz > 0){
+        funcao = (5 * num + 3) / sqrt(pow(num, 2) - 16);
+        printf("\n-> O valor da funcao f(x) eh: %.2f", funcao);
+    }
 }

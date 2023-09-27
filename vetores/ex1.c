999f686a543c1cd0e6104dc086be4d72fe3578e1
Mateus Tirulli Rozeti <129351985+Rozeti@users.noreply.github.com>
2023-09-26 21:01:31 -0300

    Add files via upload

---
+++ vetores/ex1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+main(){
+    int a[6] = {1, 0, 5, -2, -5, 7}, soma = 0;
+
+    printf("a) Os valores iniciais do vetor sao: ");
+
+    for (int i = 0; i < 6; i++){
+        printf("\n-> Posicao: %d, -> Valor: %d", i, a[i]);
+        if (i == 0 || i == 5){
+            soma += a[i];
+        }else if (i == 4){
+            a[i] = 100;
+        }
+    }
+
+    printf("\n\nb) A soma dos valores das posicoes A[0] e A[5] do vetor eh igual a: %d", soma);
+    printf("\n\nc) O novo valor da posicao A[4] no vetor eh: %d\n", a[4]);
+
+    for (int i = 0; i < 6; i++)
+        printf("\nd) O novo valor da posicao A[4] no vetor eh: %d", a[4]);
+    {
+        /* code */
+    }
+    
+    
+    
+    
+
+}

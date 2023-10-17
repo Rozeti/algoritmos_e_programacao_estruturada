7d4c215f358e47174b47ba46d9f701dfce53d857
Mateus Tirulli Rozeti <129351985+Rozeti@users.noreply.github.com>
2023-10-17 20:26:11 -0300

    Update ex2.c

--- funcoes/ex2.c
+++ funcoes/ex2.c
@@ -2,32 +2,31 @@
 
 void imprimir(int num){
     if (num == 1){
-        printf("O mes que voce escolheu foi Janeiro, com 31 dias.");
+        printf("Mes: Janeiro, com 31 dias.");
     }else if (num == 2){
-        printf("O mes que voce escolheu foi Fevereiro, com 28 dias.");
+        printf("Mes: Fevereiro, com 28 dias.");
     }else if (num == 3){
-        printf("O mes que voce escolheu foi Marco, com 31 dias.");
+        printf("Mes: Marco, com 31 dias.");
     }else if (num == 4){
-        printf("O mes que voce escolheu foi Abril, com 30 dias.");
+        printf("Mes: Abril, com 30 dias.");
     }else if (num == 5){
-        printf("O mes que voce escolheu foi Maio, com 31 dias.");
+        printf("Mes: Maio, com 31 dias.");
     }else if (num == 6){
-        printf("O mes que voce escolheu foi Junho, com 30 dias.");
+        printf("Mes: Junho, com 30 dias.");
     }else if (num == 7){
-        printf("O mes que voce escolheu foi Julho, com 31 dias.");
+        printf("Mes: Julho, com 31 dias.");
     }else if (num == 8){
-        printf("O mes que voce escolheu foi Agosto, com 31 dias.");
+        printf("Mes: Agosto, com 31 dias.");
     }else if (num == 9){
-        printf("O mes que voce escolheu foi Setembro, com 30 dias.");
+        printf("Mes: Setembro, com 30 dias.");
     }else if (num == 10){
-        printf("O mes que voce escolheu foi Outubro, com 31 dias.");
+        printf("Mes: Outubro, com 31 dias.");
     }else if (num == 11){
-        printf("O mes que voce escolheu foi Novembro, com 30 dias.");
+        printf("Mes: Novembro, com 30 dias.");
     }else if (num == 12){
-        printf("O mes que voce escolheu foi Dezembro, com 31 dias.");
+        printf("Mes: Dezembro, com 31 dias.");
     }
 }
-
 main(){
     int num;
     
@@ -37,7 +36,6 @@ main(){
         printf("\n#Valor Invalido. Pf digite um numero de 1 a 12: ");
         scanf("%d", &num);
     }
-    
     printf("\n\n");
     imprimir(num);
 }

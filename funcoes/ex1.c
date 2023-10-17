989ca76c6f1a1b71282b7d82168bdecce453a9bc
Mateus Tirulli Rozeti <129351985+Rozeti@users.noreply.github.com>
2023-10-17 19:37:08 -0300

    Update ex1.c

--- funcoes/ex1.c
+++ funcoes/ex1.c
@@ -1,2 +1,22 @@
 #include<stdio.h>
 
+float maior(float n1, float n2){
+    if (n1 > n2){
+        return n1;
+    }else{
+        return n2;
+    }    
+}
+
+main(){
+    float numero1, numero2;
+
+    printf("\n--> Digite o primeiro numero: ");
+    scanf("%f", &numero1);
+    printf("\n--> Digite o segundo numero: ");
+    scanf("%f", &numero2);
+
+    float retornoMaior = maior(numero1, numero2);
+
+    printf("\n--> O maior numero eh: %.2f", retornoMaior);
+}

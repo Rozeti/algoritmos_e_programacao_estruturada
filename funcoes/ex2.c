a569f337962902716551c1a0406fdcb83d5e820a
Mateus Tirulli Rozeti <129351985+Rozeti@users.noreply.github.com>
2023-10-17 20:30:13 -0300

    Update ex2.c

--- funcoes/ex2.c
+++ funcoes/ex2.c
@@ -1,30 +1,45 @@
 #include<stdio.h>
 
 void imprimir(int num){
-    if (num == 1){
+    switch (num){
+    case 1:
         printf("Mes: Janeiro, com 31 dias.");
-    }else if (num == 2){
+        break;
+    case 2:
         printf("Mes: Fevereiro, com 28 dias.");
-    }else if (num == 3){
+        break;
+    case 3:
         printf("Mes: Marco, com 31 dias.");
-    }else if (num == 4){
+        break;
+    case 4:
         printf("Mes: Abril, com 30 dias.");
-    }else if (num == 5){
+        break;
+    case 5:
         printf("Mes: Maio, com 31 dias.");
-    }else if (num == 6){
+        break;
+    case 6:
         printf("Mes: Junho, com 30 dias.");
-    }else if (num == 7){
+        break;
+    case 7:
         printf("Mes: Julho, com 31 dias.");
-    }else if (num == 8){
+        break;
+    case 8:
         printf("Mes: Agosto, com 31 dias.");
-    }else if (num == 9){
+        break;
+    case 9:
         printf("Mes: Setembro, com 30 dias.");
-    }else if (num == 10){
+        break;
+    case 10:
         printf("Mes: Outubro, com 31 dias.");
-    }else if (num == 11){
+        break;
+    case 11:
         printf("Mes: Novembro, com 30 dias.");
-    }else if (num == 12){
+        break;
+    case 12:
         printf("Mes: Dezembro, com 31 dias.");
+        break;
+    default:
+        break;
     }
 }
 main(){
@@ -36,6 +51,7 @@ main(){
         printf("\n#Valor Invalido. Pf digite um numero de 1 a 12: ");
         scanf("%d", &num);
     }
+    
     printf("\n\n");
     imprimir(num);
 }

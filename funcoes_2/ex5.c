2d489a05a4c698460e1e7bafbb9a942a3310bde5
Mateus Tirulli Rozeti <129351985+Rozeti@users.noreply.github.com>
2023-10-31 20:11:14 -0300

    Update ex5.c

--- funcoes_2/ex5.c
+++ funcoes_2/ex5.c
@@ -5,15 +5,41 @@ int diasMes(int mes){
     return diasMes[mes - 1];
 }
 
+void calcularIdade(int dia, int mes, int ano, int diaAtual, int mesAtual, int anoAtual){
+    int totalAnos, totalMes, totalDias;
+    if (diaAtual >= dia){
+        totalDias = diaAtual - dia;
+    }else{
+        totalDias = diaAtual + diasMes(mes) - dia;
+        mesAtual--;
+    }
+    if (mesAtual >= mes){
+        totalMes = mesAtual - mes;
+    }else{
+        totalMes = mesAtual + 12 - mes;
+        anoAtual--;
+    }
+    totalAnos = anoAtual - ano;
+    
+    printf("Idade: %d, meses: %d, dias: %d", totalAnos, totalMes, totalDias);
+}
+
 main(){
     int dia, mes, ano;
     int diaAtual, mesAtual, anoAtual;
 
     printf("-> Informe a sua data de nascimento: ");
-    scanf("%d, %d, %d", &dia, &mes, &ano);
+    scanf("%d", &dia);
+    scanf("%d", &mes);
+    scanf("%d", &ano);
+    fflush(stdin);
 
     printf("\n");
 
     printf("-> Informe a data atual: ");
-    scanf("%d, %d, %d", &diaAtual, &mesAtual, &anoAtual);
+    scanf("%d", &diaAtual);
+    scanf("%d", &mesAtual);
+    scanf("%d", &anoAtual);
+
+    calcularIdade(dia, mes, ano, diaAtual, mesAtual, anoAtual);
 }
